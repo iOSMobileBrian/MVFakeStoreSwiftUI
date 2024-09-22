7aac38640f4dd25104deeb55bce4f88eb10c321e
Brian Surface <brian.surface@outlook.com>
2024-09-21 21:16:19 -0400

    Initial Commit

---
+++ MVFakeNewsSwiftUI/MVFakeNewsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVFakeNewsSwiftUIApp.swift
+//  MVFakeNewsSwiftUI
+//
+//  Created by Brian Surface on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MVFakeNewsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
